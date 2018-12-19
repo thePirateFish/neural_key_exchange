c58b3084dd282f8b0ac830779aa109910820cfb0
thePirateFish <mbgigliotti@gmail.com>
2018-12-19 13:17:05 -0500

    update rules work

--- synch.py
+++ synch.py
@@ -1,54 +1,77 @@
+# reference: https://github.com/farizrahman4u/neuralkey
+
 import torch
 from tpm import TreeParityMachine
-#import keras
-#from keras.models import Sequential
-#from keras.layers import LocallyConnected1D, Reshape
 import numpy as np
+from timeit import default_timer as timer
+import matplotlib.pyplot as plt
 
-i = 3
+i = 0
+matches = 0
+percent_synced = 0
+sync_list = []
 
 # K = number of hidden layer nodes
 # N = number of inputs per hidden node
 # L = [-L, +L] range each weight may take
-
 k = 3
-n = 4
+n = 100
 l = 3
-learning_rate = 1.0
-tpm_alice = TreeParityMachine(k, n, l, learning_rate)
-tpm_bob = TreeParityMachine(k, n, l, learning_rate)
-# model = Sequential()
-# model.add(Reshape(input_shape=(12,), target_shape=(12,1)))
-# model.add(LocallyConnected1D(filters=3, kernel_size=n, strides=n,))
+update_rule = "hebbian"
+#update_rule = "anti-hebbian"
+#update_rule = "random walk"
+update_rules = ["hebbian", "anti-hebbian", "random walk"]
 
-# model.compile(loss='binary_crossentropy',
-#               optimizer='rmsprop',
-#               metrics=['accuracy'])
+def calculate_percent_synced(wa, wb):
+	result = ((1.0 - torch.mean(1.0 * torch.abs(wa - wb) / (2*l))).item()) * 100
+	return round(result, 3)
 
-torch.manual_seed(2)
-while(i > 0):
-	# 1 x k*n tensor of random ints, either -1, 0, or 1
-	#rand_array = np.random.randint(0, high=2, size=(1, k*n))
-	#rand_array[rand_array==0] = -1
-	#print(rand_array)
-	#rand_input = torch.tensor(rand_array)
-	
+def generate_random_input():
 	r_input = torch.randint(-1, 2, (1, k*n))
 	r_input[r_input==0] = -1
+	return r_input
 
-	# #print(rand_input)
-	# tpm_alice.output()
-	# tpm_bob.output()
-	result_a = tpm_alice(r_input)
-	result_b = tpm_bob(r_input)
-	# print(result_a)
-	# print(result_b)
-	if (result_a == result_b):
-		print("MATCH")
-	# else:
-	# 	print("NO MATCH")
-	# print("\n")
-	i-=1
+tpm_alice = TreeParityMachine(k, n, l)
+tpm_bob = TreeParityMachine(k, n, l)
+
+#torch.manual_seed(2)
+start = timer()
+while(percent_synced != 100):
+	r_input = generate_random_input()
+	tau_a = tpm_alice(r_input)
+	tau_b = tpm_bob(r_input)
+
+	if (tau_a == tau_b):
+		tpm_alice.update(tau_b, rule=update_rule)
+		tpm_bob.update(tau_a, rule=update_rule)
+		wa = tpm_alice.get_weights()
+		wb = tpm_bob.get_weights()
+		percent_synced = calculate_percent_synced(wa, wb)
+		matches+=1
+
+	sync_list.append(percent_synced)
+
+	if (i % 50 == 0 or percent_synced == 1):
+		print("Percent synced: %.2f" % percent_synced)
+
+	i+=1
+
+end = timer()
+print("Number of iterations: %d" % i)
+print("Elapsed time: %.2f" % (end-start))
+print("Matches: %d" % matches)
+print("alice weights: %s" % wa)
+print("bob weights: %s" % wb)
+#print(sync_list)
+style = 'Solarize_Light2'
+with plt.style.context(style):
+	timesteps = np.arange(0, i, 1)
+	fig, ax = plt.subplots(num=style)
+	ax.plot(timesteps, sync_list)
+	ax.set(xlabel='Learning Steps', ylabel='% Synchronized', title='Synchronization Example Using Hebbian Learning')
+
+plt.show()
+
 
 # rand_input = np.random.randint(-1, 2, size=k*n)
 # rand_output = np.random.randint(-1, 2, size=1)
--- tpm.py
+++ tpm.py
@@ -1,73 +1,99 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
 import numpy as np
+from torch.optim.optimizer import Optimizer, required
+
+
+# k = 3
+# n = 4
+# l = 3
+
 
 class TreeParityMachine(nn.Module):
 
 	# num_hidden_layer_nodes = number of hidden layer nodes
 	# num_inputs_per_node = number of inputs per hidden node
 	# L = [-L, +L] range each weight may tanum_hidden_layer_nodese
-	# lr = learning rate
-	def __init__(self, num_hidden_layer_nodes=3, num_inputs_per_node=4, weight_range=6, lr=1.0):
+	def __init__(self, k=3, n=4, l=3):
 		super(TreeParityMachine, self).__init__()
-		#self.initialize_seed(2)
 
-		self.num_hidden_layer_nodes = num_hidden_layer_nodes
-		self.num_inputs_per_node = num_inputs_per_node
-		self.weight_range = weight_range
-		self.hidden = nn.ModuleList()
-		self.sigmas = torch.zeros([1, 3], dtype=torch.int32)
+		self.num_hidden_layer_nodes = k
+		self.num_inputs_per_node = n
+		self.weight_range = l
+		self.update_rules = ["hebbian", "anti-hebbian", "random walk"]
+
+
+		self.register_parameter("weights", nn.Parameter(torch.randint(-1*self.weight_range, self.weight_range+1, (1, self.num_hidden_layer_nodes*self.num_inputs_per_node)).float(), requires_grad=False))
+		self.register_parameter("sigmas", nn.Parameter(torch.zeros([self.num_hidden_layer_nodes]), requires_grad=False))
+		self.register_parameter("tau", nn.Parameter(torch.zeros([1]), requires_grad=False))
+		self.last_input = torch.empty([1, self.num_hidden_layer_nodes*self.num_inputs_per_node])
 		
-
-		for num_hidden_layer_nodes in range(num_hidden_layer_nodes):
-			self.hidden.append(nn.Linear(self.num_inputs_per_node, 1))
-			torch.nn.init.uniform_(self.hidden[num_hidden_layer_nodes].weight, a=(-1)*self.weight_range, b=self.weight_range+1)
-
-		#self.weights_layer = torch.nn.Linear(1, num_hidden_layer_nodes, num_inputs_per_node)
-		# self.weights_layer = torch.nn.Conv1d(1, 1, kernel_size=self.n, stride=self.n, groups=self.num_hidden_layer_nodes)
-		# torch.nn.init.constant_(self.weights_layer.weight, 1)
-
-		# self.linears = nn.ModuleList()
-
-		# for i in range(self.num_hidden_layer_nodes):
-		# 	linears.append(torch.nn.Linear(self.n, 1))
-		# for x, l in enumerate(self.linears):
-		# 	torch.nn.init.constant_(l.weight, 1)
 
 	def forward(self, x):
 
 		# tau = output of hidden layer
-		#for param in self.weights_layer.parameters():
-		#	print(param.data)
-		# print(self.weights_layer.weight)
-		# print(x)
-		# x = x.reshape(1, self.num_hidden_layer_nodes, self.n)
-		# print(x)
-		# x = self.weights_layer(x)
-		# print(x)
-		# x = x.sign()
-
-		#x = torch.split(x, self.num_hidden_layer_nodes, dim=1)
+		self.last_input = x
 		y = torch.chunk(x, self.num_hidden_layer_nodes, dim=1)
 		#print(type(y))
 		parts = list(y)
-		#print(parts)
-		for i, l in enumerate(self.hidden):
-			#print(parts[i])
-			parts[i] = l(parts[i])
-			#print(parts[i])
-			#parts[i] = parts[i].sign_()
-			self.sigmas = parts[i].sign_()
-			#print(parts[i])
 
-		z = torch.tensor(self.sigmas)
-		#print(z)
-		tau = torch.prod(z)
-		print(tau.item())
-		#print("hello")
-		return tau.item()
+		weight_chunks = torch.chunk(self.weights, self.num_hidden_layer_nodes, dim=1)
 
+		for i, weight_chunk in enumerate(weight_chunks):
+			self.sigmas[i] = weight_chunk.mm(parts[i].transpose(0,1).float()).sign_()
+		self.sigmas[self.sigmas==0] = -1
+
+		self.tau[0] = torch.prod(self.sigmas)
+
+		return self.tau.item()
+
+	def print_parameters(self):
+		for name, param in self.named_parameters():
+			print("Name: %s" % name)
+			print(param.data)
+
+	def print_layer(self, id, node):
+		print("Node %d" % id)
+		print("\t%s" % node)
+		print("\tWeights: %s" % node.weight.data)
+
+	def update(self, other_tau=required, rule=required):
+		if rule not in self.update_rules:
+			raise ValueError("Update rule must be either hebbian, anti-hebbian, or random walk.")
+
+		update_factor = 0
+		for i, sigma in enumerate(self.sigmas):
+			for j in range(self.num_inputs_per_node):
+				index = i*self.num_inputs_per_node + j
+			
+
+				# Activation: we only update when sigma = tau = other_tau
+				activation = theta(sigma.item(), self.tau.item()) * theta(self.tau.item(), other_tau)
+				if (activation == 0):
+					return
+
+				if (rule == "hebbian"):
+					update_factor = self.last_input[0][index] * self.tau.item() * activation
+					self.weights[0][index] += update_factor
+				elif (rule == "anti-hebbian"):
+					update_factor = self.last_input[0][index] * self.tau.item() * activation
+					self.weights[0][index] -= update_factor
+				elif (rule == "random walk"):
+					update_factor = self.last_input[0][index] * activation
+					self.weights[0][index] += update_factor
+
+				
+		self.weights.clamp_(min=(-1*self.weight_range), max=(self.weight_range))
+
+	def get_weights(self):
+		for name, param in self.named_parameters():
+			if name in ['weights']:
+				return param
+
+
+	# TESTING
+	# intended for self-generating input
 	def output(self):
 		#np.random.seed(seed=2)
 		rand_array = np.random.randint(0, high=2, size=(1, self.num_hidden_layer_nodes*self.num_inputs_per_node))
@@ -88,6 +114,27 @@ class TreeParityMachine(nn.Module):
 
 	# def update_hebbian(self, partner_tau):
 
+class TPM_Update(Optimizer):
+	def __init__(self, params, other_tau=required, rule="hebbian"):
+		defaults = dict(other_tau=other_tau, rule=rule)
+		super(TPM_Update, self).__init__(params, defaults)
+
+	def __setstate__(self, state):
+		super(TPM_Update, self).__setstate__(state)
+	
+	def step(self):
+		for group in self.param_groups:
+			other_tau = group['other_tau']
+			rule = group['rule']
+
+			for p in group['params']:
+				print(p)
+
+def theta(a, b):
+	if (a==b):
+		return 1
+	else:
+		return 0
 
 
 
