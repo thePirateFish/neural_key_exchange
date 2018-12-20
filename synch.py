1f490db5cabfc478abe6acb99df549186d5cea38
thePirateFish <mbgigliotti@gmail.com>
2018-12-20 15:16:07 -0500

    styling

--- synch.py
+++ synch.py
@@ -1,4 +1,8 @@
-# reference: https://github.com/farizrahman4u/neuralkey
+# Author: Matthew Gigliotti
+# Fall 2018
+# 
+# reference: credit to https://github.com/farizrahman4u/neuralkey
+# for calculating synchronizations
 
 import torch
 from tpm import TreeParityMachine
@@ -11,16 +15,18 @@ matches = 0
 percent_synced = 0
 sync_list = []
 
+# Default settings
 # K = number of hidden layer nodes
 # N = number of inputs per hidden node
 # L = [-L, +L] range each weight may take
 k = 3
-n = 100
+n = 4
 l = 3
+
 update_rule = "hebbian"
 #update_rule = "anti-hebbian"
 #update_rule = "random walk"
-update_rules = ["hebbian", "anti-hebbian", "random walk"]
+#update_rules = ["hebbian", "anti-hebbian", "random walk"]
 
 def calculate_percent_synced(wa, wb):
 	result = ((1.0 - torch.mean(1.0 * torch.abs(wa - wb) / (2*l))).item()) * 100
@@ -51,7 +57,7 @@ while(percent_synced != 100):
 
 	sync_list.append(percent_synced)
 
-	if (i % 50 == 0 or percent_synced == 1):
+	if (i % 10 == 0 or percent_synced == 1):
 		print("Percent synced: %.2f" % percent_synced)
 
 	i+=1
@@ -60,23 +66,15 @@ end = timer()
 print("Number of iterations: %d" % i)
 print("Elapsed time: %.2f" % (end-start))
 print("Matches: %d" % matches)
-print("alice weights: %s" % wa)
-print("bob weights: %s" % wb)
+print("Alice's final weights: %s" % wa)
+print("Bob's final weights: %s" % wb)
 #print(sync_list)
-style = 'Solarize_Light2'
+style = "Solarize_Light2"
 with plt.style.context(style):
 	timesteps = np.arange(0, i, 1)
 	fig, ax = plt.subplots(num=style)
 	ax.plot(timesteps, sync_list)
-	ax.set(xlabel='Learning Steps', ylabel='% Synchronized', title='Synchronization Example Using Hebbian Learning')
+	ax.set(xlabel="Learning Steps", ylabel="% Synchronized", title="Synchronization Example Using %s Learning" % update_rule.title())
 
 plt.show()
 
-
-# rand_input = np.random.randint(-1, 2, size=k*n)
-# rand_output = np.random.randint(-1, 2, size=1)
-
-# x_val = np.random.randint(-1, 2, size=k*n)
-# y_val = np.random.randint(-1, 2, size=1).reshape(1, 1)
-# print(rand_input.shape)
-# model.fit(rand_input, rand_output, batch_size=1, epochs=1, validation_data=(x_val, y_val))
--- tpm.py
+++ tpm.py
@@ -1,119 +1,166 @@
-import torch 
-import torch.nn as nn 
-import torch.nn.functional as F 
+# Author: Matthew Gigliotti
+# Fall 2018
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 from torch.optim.optimizer import Optimizer, required
 
+"""
+An implementation of a Tree Parity Machine as a PyTorch nn.Module.
+Default values:
+	k = 3	->	The number of hidden layer nodes
+	n = 4	->	Inputs per hidden node
+	l = 3	->	Synaptic depth
 
-# k = 3
-# n = 4
-# l = 3
+An input vector must be a 1 x (k*n) tensor of 1's and -1's
+The default input vector shape is 1 x 12, with 4 inputs to each hidden node.
+Weights may take a range [-l, +l], and will be clamped to stay in that range.
 
-
+"""
 class TreeParityMachine(nn.Module):
-
-	# num_hidden_layer_nodes = number of hidden layer nodes
-	# num_inputs_per_node = number of inputs per hidden node
-	# L = [-L, +L] range each weight may tanum_hidden_layer_nodese
+    
 	def __init__(self, k=3, n=4, l=3):
 		super(TreeParityMachine, self).__init__()
 
 		self.num_hidden_layer_nodes = k
 		self.num_inputs_per_node = n
 		self.weight_range = l
-		self.update_rules = ["hebbian", "anti-hebbian", "random walk"]
+		self.update_rules = ["hebbian", "anti-hebbian",
+		"random walk"]
 
+		input_length = self.num_hidden_layer_nodes*self.num_inputs_per_node
 
-		self.register_parameter("weights", nn.Parameter(torch.randint(-1*self.weight_range, self.weight_range+1, (1, self.num_hidden_layer_nodes*self.num_inputs_per_node)).float(), requires_grad=False))
-		self.register_parameter("sigmas", nn.Parameter(torch.zeros([self.num_hidden_layer_nodes]), requires_grad=False))
-		self.register_parameter("tau", nn.Parameter(torch.zeros([1]), requires_grad=False))
-		self.last_input = torch.empty([1, self.num_hidden_layer_nodes*self.num_inputs_per_node])
+		self.register_parameter("weights",
+		nn.Parameter(torch.randint(-1*self.weight_range,
+									self.weight_range+1,
+							 		(1, input_length)).float(),
+									requires_grad=False)) 
+
+		self.register_parameter("sigmas",
+		nn.Parameter(torch.zeros([self.num_hidden_layer_nodes]),
+								requires_grad=False)) 
+
+		self.register_parameter("tau",
+		nn.Parameter(torch.zeros([1]), requires_grad=False)) 
+
+		self.last_input = torch.empty([1, input_length])
 		
 
 	def forward(self, x):
-
-		# tau = output of hidden layer
 		self.last_input = x
 		y = torch.chunk(x, self.num_hidden_layer_nodes, dim=1)
-		#print(type(y))
 		parts = list(y)
-
-		weight_chunks = torch.chunk(self.weights, self.num_hidden_layer_nodes, dim=1)
+		weight_chunks = torch.chunk(self.weights,
+									self.num_hidden_layer_nodes,
+									dim=1)
 
 		for i, weight_chunk in enumerate(weight_chunks):
-			self.sigmas[i] = weight_chunk.mm(parts[i].transpose(0,1).float()).sign_()
+			self.sigmas[i] = \
+			weight_chunk.mm(parts[i].transpose(0,1).float()).sign_()
 		self.sigmas[self.sigmas==0] = -1
 
+		# tau = output of hidden layer
 		self.tau[0] = torch.prod(self.sigmas)
 
 		return self.tau.item()
 
-	def print_parameters(self):
+	def print_parameters(self): 
 		for name, param in self.named_parameters():
 			print("Name: %s" % name)
 			print(param.data)
 
-	def print_layer(self, id, node):
+	def print_layer(self, id, node): 
 		print("Node %d" % id)
 		print("\t%s" % node)
 		print("\tWeights: %s" % node.weight.data)
 
 	def update(self, other_tau=required, rule=required):
 		if rule not in self.update_rules:
-			raise ValueError("Update rule must be either hebbian, anti-hebbian, or random walk.")
+			raise ValueError(
+		"Update rule must be either hebbian, anti-hebbian, or random walk.")
 
 		update_factor = 0
 		for i, sigma in enumerate(self.sigmas):
 			for j in range(self.num_inputs_per_node):
 				index = i*self.num_inputs_per_node + j
-			
-
-				# Activation: we only update when sigma = tau = other_tau
-				activation = theta(sigma.item(), self.tau.item()) * theta(self.tau.item(), other_tau)
+				activation = theta(sigma.item(), self.tau.item())* \
+							 theta(self.tau.item(), other_tau)
+				
 				if (activation == 0):
 					return
 
 				if (rule == "hebbian"):
-					update_factor = self.last_input[0][index] * self.tau.item() * activation
+					update_factor = self.last_input[0][index]* \
+					self.tau.item()*activation
 					self.weights[0][index] += update_factor
 				elif (rule == "anti-hebbian"):
-					update_factor = self.last_input[0][index] * self.tau.item() * activation
-					self.weights[0][index] -= update_factor
+					update_factor = self.last_input[0][index]* \
+					self.tau.item()*activation
+					self.weights[0][index] -= update_factor 
 				elif (rule == "random walk"):
-					update_factor = self.last_input[0][index] * activation
+					update_factor = self.last_input[0][index]*activation
 					self.weights[0][index] += update_factor
 
 				
-		self.weights.clamp_(min=(-1*self.weight_range), max=(self.weight_range))
+		self.weights.clamp_(min=(-1*self.weight_range),
+							max=(self.weight_range))
 
 	def get_weights(self):
 		for name, param in self.named_parameters():
 			if name in ['weights']:
 				return param
 
+""" Returns 1 if values are equal, 0 otherwise.
+Used to determine the weight updates, based on the agreement
+of both tau values, and the tau and sigma values.
 
-	# TESTING
-	# intended for self-generating input
-	def output(self):
-		#np.random.seed(seed=2)
-		rand_array = np.random.randint(0, high=2, size=(1, self.num_hidden_layer_nodes*self.num_inputs_per_node))
-		rand_array[rand_array==0] = -1
-		#print(rand_array)
-		r_input = torch.tensor(rand_array)
-		#print(np.random.rand(4))
-		# r_input = torch.randint(0, 2, (1, self.num_hidden_layer_nodes*self.num_inputs_per_node))
-		# r_input[r_input==0] = -1
-		#print(r_input)
-		self.forward(r_input)
-		#print(self.forward(r_input))
+"""
+def theta(a, b):
+    if (a==b):
+    	return 1
+    else:
+    	return 0
 
-	def initialize_seed(self, seed):
-		#torch.manual_seed(seed)
-		np.random.seed(seed=2)
-	# def update(self, partner_tau, update_rule):
+""" Return a random input vector
 
-	# def update_hebbian(self, partner_tau):
+Under development. Intended to be a standalone input generator
+such that each party in the protocol can generate the same
+random sequence individually, Given an agreed upon random seed.
+This may not be possible.
 
+"""   
+# def output(self):
+# np.random.seed(seed=2)         
+# rand_array = np.random.randint(0, high=2,
+# size=(1, self.num_hidden_layer_nodes*self.num_inputs_per_node))
+# rand_array[rand_array==0] = -1         
+# print(rand_array)         
+# r_input = torch.tensor(rand_array)
+# print(np.random.rand(4))         
+# r_input = torch.randint(
+#	0, 2, (1, self.num_hidden_layer_nodes*self.num_inputs_per_node))
+# r_input[r_input==0] = -1
+# print(r_input)
+# self.forward(r_input)
+# print(self.forward(r_input))
+
+""" NOT USED
+Sets the random seed. Used for setting the seed with numpy vs. PT.
+
+"""
+def initialize_seed(self, seed):
+	np.random.seed(seed=2)
+
+""" NOT USED
+
+Under development.
+A custom optimizer for a TPM performing synchronization. Determined to
+not be worth using, since there is no real loss function. The updates
+can be more easily performed manually using the update method.
+
+"""
 class TPM_Update(Optimizer):
 	def __init__(self, params, other_tau=required, rule="hebbian"):
 		defaults = dict(other_tau=other_tau, rule=rule)
@@ -129,15 +176,4 @@ class TPM_Update(Optimizer):
 
 			for p in group['params']:
 				print(p)
-
-def theta(a, b):
-	if (a==b):
-		return 1
-	else:
-		return 0
-
-
-
-# define loss function
-# define optimizer
 
